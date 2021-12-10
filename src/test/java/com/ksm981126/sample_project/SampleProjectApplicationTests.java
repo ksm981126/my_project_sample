1acb4506681800dc02bde0d2070c0311280779fa
ksm981126 <ksm981126@naver.com>
2021-12-10 10:28:19 +0900

    첫 번째 프로젝트 수정

---
+++ src/test/java/com/ksm981126/sample_project/SampleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksm981126.sample_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
